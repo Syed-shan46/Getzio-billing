af145fa0fb734399ccd0158de7438003aab4be5f
syed <shihadpalakkad@MacBookPro.lan>
2026-06-27 17:55:09 +0530

    fix: Rewrite themeModeProvider using Notifier to resolve Riverpod v3 compilation errors

--- lib/core/theme/theme_provider.dart
+++ lib/core/theme/theme_provider.dart
@@ -2,24 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:getzio_billing/core/storage/hive_service.dart';
 
-class ThemeModeNotifier extends StateNotifier<ThemeMode> {
-  final HiveService _hiveService;
+class ThemeModeNotifier extends Notifier<ThemeMode> {
   static const String _themeKey = 'selected_theme_mode';
 
-  ThemeModeNotifier(this._hiveService) : super(ThemeMode.system) {
-    final storedIndex = _hiveService.getPreference(_themeKey, defaultValue: -1);
+  @override
+  ThemeMode build() {
+    final hiveService = ref.watch(hiveServiceProvider);
+    final storedIndex = hiveService.getPreference(_themeKey, defaultValue: -1);
     if (storedIndex >= 0 && storedIndex < ThemeMode.values.length) {
-      state = ThemeMode.values[storedIndex];
+      return ThemeMode.values[storedIndex];
     }
+    return ThemeMode.system;
   }
 
   void setThemeMode(ThemeMode mode) {
     state = mode;
-    _hiveService.savePreference(_themeKey, mode.index);
+    final hiveService = ref.read(hiveServiceProvider);
+    hiveService.savePreference(_themeKey, mode.index);
   }
 }
 
-final themeModeProvider = StateNotifierProvider<ThemeModeNotifier, ThemeMode>((ref) {
-  final hive = ref.watch(hiveServiceProvider);
-  return ThemeModeNotifier(hive);
-});
+final themeModeProvider = NotifierProvider<ThemeModeNotifier, ThemeMode>(ThemeModeNotifier.new);
